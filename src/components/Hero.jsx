46cb29ff231c6ee8a7849e73211ed46b57aa31e0
Gilmar Viana <70184391+gilmarviana@users.noreply.github.com>
2025-07-08 12:00:08 -0300

    .

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,10 +6,10 @@ const whatsappLink = "https://api.whatsapp.com/send?phone=5519998305956&text=Ol%
 const Hero = () => (
   <section className="hero" id="inicio">
     <h1>A Melhor lista de canais, filmes e séries.</h1>
-    <p>Teste agora mesmo a melhor lista de canais, filmes e séries do Brasil, 4 horas 100% grátis</p>
+    <p>Teste agora mesmo a melhor lista de canais, filmes e séries do Brasil, 12 horas 100% grátis</p>
     <a href={whatsappLink} className="hero__btn" target="_blank" rel="noopener noreferrer">Teste Agora</a>
     <div className="hero__promo">PROMOÇÃO IPTV Barato</div>
   </section>
 );
 
-export default Hero; 
+export default Hero;
